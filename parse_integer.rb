1d8100733086b3719ce3f62b4531dd0c35179d8f
Ralph Hartman <ralph_e@hotmail.com>
2021-07-02 01:51:04 +0000

    still trying to solve the parse integer method

--- parse_integer.rb
+++ parse_integer.rb
@@ -1,3 +1,10 @@
+# we have an array called result that contains all of the words in integer format
+# we need to iterate through this array and add the integers together
+# except if the integer is a 100 or a 1000 we need to multiply it by the
+#   previous integer and then store it in a separate array.
+# after we have all these integers we need to sum them up and return...
+# the example and test case we need to figure out is 666666...
+
 NUMS = { "zero" => 0, "one" => 1, "two" => 2, "three" => 3, "four" => 4,
         "five" => 5, "six" => 6, "seven" => 7, "eight" => 8, "nine" => 9,
         "ten" => 10, "eleven" => 11, "twelve" => 12, "thirteen" => 13,
@@ -9,8 +16,7 @@ NUMS = { "zero" => 0, "one" => 1, "two" => 2, "three" => 3, "four" => 4,
 def parse_int(string)
   p string
   result = []
-  small_nums = []
-  total = 0
+  total = []
   array = string.split(/\W/)
   array.each do |word|
     NUMS.each do |key, value|
@@ -18,16 +24,9 @@ def parse_int(string)
     end
   end
   result.each_with_index do |value, idx|
-    unless (value == 100) || (value == 1000)
-    small_nums << value
-    small_nums.replace([small_nums.sum])
-    else total += (small_nums.pop * value)
-    end
+    
   end
-  total += (small_nums.sum)
-  total
 end
-
 p parse_int("one thousand three hundred thirty-seven") == 1337
 p parse_int("six thousand six hundred and sixty-eight") == 6668
 p parse_int("six hundred sixty-six thousand six hundred sixty-six") == 666666
