fa51965bbe4bad43c79adb0cfe75e2855aad0541
Ralph Hartman <ralph_e@hotmail.com>
2021-07-06 00:20:34 +0000

    working on drills to practice common complex problems

--- DRILLS.rb
+++ DRILLS.rb
@@ -1,15 +1,39 @@
 # Common complex things to practice:
+# => find all sub_strings from a string
+# => find all sub_arrays from an array 
+
 
 # PROBLEM:
 # => Find all possible sub_strings in a given string that contain 2 or more letters.
 
+# STEPS:
+# initialize a result variable and assign it to an empty array
+# initialize a variable to track the index we are currently on
+# use until loop with a break condition of index variable being equal to the size of the string - 1. This will ensure we loop through first letter to second to last character.
+# within this loop initialize a variable called length and assign it to the integer object 2. This will ensure we start with the correct sub_string length.
+# use another until loop with a break condition of length being greater than the size of the string minus the index. So if the length of the string is 4 and we are currently on index 2 which is the second to last character we will only execute the loop one time.
+# append to the results array the sub_strings as denoted by the index and length.
+# increment length after each inner loop
+# increment index after each outer loop
+# return the result array
+
+
 def find_substrings(string)
   result = []
-  
+  index = 0
+  until index == string.size - 1
+    length = 2
+    until length > (string.size - index)
+      result << string[index, length]
+      length += 1
+    end
+    index += 1
+  end
+  p result
 end
 
 p find_substrings("halo") == ["ha", "hal", "halo", "al", "alo", "lo"]
-
+p find_substrings("ab") == ["ab"]
 
 
 
--- practice_live.rb
+++ practice_live.rb
@@ -1,34 +1,72 @@
 # Given an array of n positive integers and a positive integer s, find the minimal length of a contiguous subarray of which the sum ≥ s. If there isn't one, return 0 instead.
 
-# In my own words... we are given an array of n pos integers and a pos integer s.
-# I need to find the minimal length of a contiguous subarray of which the sum > s.
-# Input: So we are given an array... and an integer.
-# Output: and we need to return an integer, that is the minimal length of a contiguous sub_array.... in which the sum of this sub_array is greater than or equal to s...
+# input: an array, and an integer
+# output: an integer
 
-# Rules
-# We are finding the minimum length of a sub_array
-# where the sum of this sub_array is greater or equal to s 
+# Problem:
+# => We are given an array and we want to find the smallest sub_array that has a sum that is greater than or equal to the given integer. Return the length of sub_array. If there is none than return a 0.
 
-# Steps:
-# => initialize a holding array.
-# => iterate through the original array and append each element to the holding array.
-# => check the sum. if greater than or equal to s
-# => then store the length in a variable
-# => clear the holding array.
-# => move on to the next elements. repeat.
-# => return the length
+# Algorithm:
+# **High Level**
+# => Return 0 if the sum of the array is not greater than or equal to the given int
+# => Initialize an array to hold all available sub_arrays
+# => ** hard part ** Find all the available sub_arrays and
+# => check to see if their sums are >= the given integer
+# => add them to a result array.
+# => return the size of the one that is the smallest in length.
+
+# Psuedocode:
+  # => result = []
+  # => sub_strings = subs(array)
+  # => sub_strings.each do |sub|
+  # =>   if sub.sum >= s
+  # =>     result << sub
+  # =>   else next
+  # =>   end
+  # => end
+  # => result.min.length
+
+# method that will find all the available substrings in the given array
+def subs(array)
+  holding_array = []
+  index = 0
+  until index > (array.size - 1)
+    length = 1
+    until length > array.size - index
+    holding_array << array[index, length]
+    length += 1
+    end
+    index += 1
+  end
+  holding_array
+end
+
+# subs([1,2]) == [[1], [1,2], [2]]
+# subs([1,2,3]) == [[1], [1,2], [1,2,3], [2], [2,3], [3] ]
+
+# method that will return an array from the array of sub_strings that meets the requirements of the sum being equal to or greater than the give integer.
+def qualified(array, s)
+  result = []
+  array.each do |sub_array|
+    if sub_array.sum >= s
+      result << sub_array
+    else next
+    end
+  end
+  result
+end
 
 def minSubLength(array, s)
-  result = []
-  holding_array = []
-  array.each do |element|
-    
-    holding_array << element
-    holding_array.sum >= s ? result << holding_array.length : next
-    holding_array = []
+  return 0 if array.sum < s
+  
+  sub_strings = subs(array)
+  qualified_subs = qualified(sub_strings, s)
+  
+  answer = qualified_subs[0].length
+  qualified_subs.each do |sub_array|
+    sub_array.length < answer ? answer = sub_array.length : next
   end
-  p result
-  p result.min
+  answer
 end
 
 p minSubLength([2,3,1,2,4,3], 7) == 2
